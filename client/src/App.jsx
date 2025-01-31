9c53ae0474cf21bc87e731d356fd5c079c27f80d
Ravi Kumar <mbravikumar0@gmail.com>
2025-01-31 15:54:44 +0530

    Pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Courses from './Pages/Courses'
+import Notes from './Pages/Notes'
+import Jobs from './Pages/Jobs'
+import Dashboard from './Pages/Dashboard'
+
 export default function App(){
   return(
-    <h1 className='text-3xl text-red-600 bg-slate-800'>Hello World!</h1>
-  )
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/courses' element={<Courses />} />
+        <Route path='/notes' element={<Notes />} />
+        <Route path='/jobs' element={<Jobs />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+
+
+
+
+      </Routes>
+    </BrowserRouter>
+   
+  );
 }
